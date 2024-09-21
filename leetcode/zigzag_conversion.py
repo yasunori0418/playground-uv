361cf82d939e50e2a2a9711ad09d6f3a386e996f
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-22 01:05:40 +0900

    update: パズルが難しいのでメモメモ

--- leetcode/zigzag_conversion.py
+++ leetcode/zigzag_conversion.py
@@ -1,6 +1,55 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        pass
+        if len(s) <= 1:
+            return s
 
-assert Solution().convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR"
-assert Solution().convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI"
+
+
+assert Solution().convert("A", 1) == "A"
+
+# PAYPALISHIRING
+#
+#
+# P     A     H     N
+#
+# A  P  L  S  I  I  G
+#
+# Y     I     R  
+#
+#
+# PAHNAPLSIIGYIR
+# assert Solution().convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR"
+
+
+# PAYPALISHIRING
+#
+#
+# P        I        N
+#
+# A     L  S     I  G
+#
+# Y  A     H  R
+#
+# P        I  
+#
+#
+# PINALSIGYAHRPI
+# assert Solution().convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI"
+
+
+# PAYPALISHIRING
+#
+#
+# P           H  
+#
+# A        S  I  
+#
+# Y     I     R  
+#
+# P  L        I  G
+#
+# A           N  
+#
+#
+# PHASIYIRPLIGAN
+# assert Solution().convert("PAYPALISHIRING", 5) == "PHASIYIRPLIGAN"
