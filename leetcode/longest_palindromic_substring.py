40a2e171b3491992551677db6525e78a9977d5f6
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-21 23:46:31 +0900

    update: deepdiff便利

--- leetcode/longest_palindromic_substring.py
+++ leetcode/longest_palindromic_substring.py
@@ -1,3 +1,35 @@
+from deepdiff import DeepDiff
+
+
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         pass
+
+
+def allPatterns(s):
+    for i in range(len(s)):
+        for j in range(i, len(s)):
+            yield s[i : j + 1]
+
+
+assert not DeepDiff(
+    list(allPatterns("babad")),
+    [
+        "b",
+        "a",
+        "b",
+        "a",
+        "d",
+        "ba",
+        "ab",
+        "ba",
+        "ad",
+        "bab",
+        "aba",
+        "bad",
+        "baba",
+        "abad",
+        "babad",
+    ],
+    ignore_order=True,
+)
