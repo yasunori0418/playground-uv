9b4c77bb08278b4f59eeaed67879db39101e70a3
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-22 00:50:17 +0900

    Add: zigzag_conversion.py

---
+++ leetcode/zigzag_conversion.py
@@ -0,0 +1,6 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        pass
+
+assert Solution().convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR"
+assert Solution().convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI"
