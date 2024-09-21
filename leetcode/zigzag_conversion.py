d421a2cde0a49a574a9db49def0a88f36fca2392
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-22 02:54:28 +0900

    update: 多分、こんなのが作れたら幸せ

--- leetcode/zigzag_conversion.py
+++ leetcode/zigzag_conversion.py
@@ -22,25 +22,36 @@ assert gen_patterns("PAYPALISHIRING", 3) == [
 ]
 
 
-assert gen_patterns("PAYPALISHIRING", 4) == [
-    ["P", "A", "Y", "P"],
-    [" ", " ", "A", " "],
-    [" ", "L", " ", " "],
-    ["I", "S", "H", "I"],
-    [" ", " ", "R", " "],
-    [" ", "I", " ", " "],
-    ["N", "G", " ", " "],
-]
+assert gen_patterns("PAYPALISHIRING", 3) == {
+    1: ["P", "A", "Y"],
+    2: [" ", "P", " "],
+    3: ["A", "L", "I"],
+    4: [" ", "S", " "],
+    5: ["H", "I", "R"],
+    6: [" ", "I", " "],
+    7: ["N", "G"],
+}
 
 
-assert gen_patterns("PAYPALISHIRING", 5) == [
-    ["P", "A", "Y", "P", "A"],
-    [" ", " ", " ", "L", " "],
-    [" ", " ", "I", " ", " "],
-    [" ", "S", " ", " ", " "],
-    ["H", "I", "R", "I", "N"],
-    [" ", " ", " ", "G", " "],
-]
+assert gen_patterns("PAYPALISHIRING", 4) == {
+    1: ["P", "A", "Y", "P"],
+    2: [" ", " ", "A", " "],
+    3: [" ", "L", " ", " "],
+    4: ["I", "S", "H", "I"],
+    5: [" ", " ", "R", " "],
+    6: [" ", "I", " ", " "],
+    7: ["N", "G"],
+}
+
+
+assert gen_patterns("PAYPALISHIRING", 5) == {
+    1: ["P", "A", "Y", "P", "A"],
+    2: [" ", " ", " ", "L", " "],
+    3: [" ", " ", "I", " ", " "],
+    4: [" ", "S", " ", " ", " "],
+    5: ["H", "I", "R", "I", "N"],
+    6: [" ", " ", " ", "G"],
+}
 
 
 # assert Solution().convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR"
