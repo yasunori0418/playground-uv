e949009960bc2f59c052000eadb847dc0f7c4375
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-22 01:34:13 +0900

    update: 二重配列を作る作戦で行く…ためのテストコード

--- leetcode/zigzag_conversion.py
+++ leetcode/zigzag_conversion.py
@@ -4,61 +4,37 @@ class Solution:
             return s
 
 
-
 assert Solution().convert("A", 1) == "A"
 
-# PAYPALISHIRING
-#
-# 1     5     9     13
-# P     A     H     N
-#
-# 2  4  6  8  10 12 14
-# A  P  L  S  I  I  G
-#
-# 3     7     11
-# Y     I     R
-#
-#
-# PAHNAPLSIIGYIR
+
+def gen_patterns(string: str, numRows):
+    pass
+
+
+assert gen_patterns("PAYPALISHIRING", 3) == [
+    ["P", " ", "A", " ", "H", " ", "N"],
+    ["A", "P", "L", "S", "I", "I", "G"],
+    ["Y", " ", "I", " ", "R", " ", " "],
+]
+
+
+assert gen_patterns("PAYPALISHIRING", 4) == [
+    ["P", " ", " ", "I", " ", " ", "N"],
+    ["A", " ", "L", "S", " ", "I", "G"],
+    ["Y", "A", " ", "H", "R", " ", " "],
+    ["P", " ", " ", "I", " ", " ", " "],
+]
+
+
+assert gen_patterns("PAYPALISHIRING", 5) == [
+    ["P", " ", " ", " ", "H", " "],
+    ["A", " ", " ", "S", "I", " "],
+    ["Y", " ", "I", " ", "R", " "],
+    ["P", "L", " ", " ", "I", "G"],
+    ["A", " ", " ", " ", "N", " "],
+]
+
+
 # assert Solution().convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR"
-
-
-# PAYPALISHIRING
-#
-# 1        7        13
-# P        I        N
-#
-# 2     6  8     12 14
-# A     L  S     I  G
-#
-# 3  5     9  11
-# Y  A     H  R
-#
-# 4        10
-# P        I
-#
-#
-# PINALSIGYAHRPI
 # assert Solution().convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI"
-
-
-# PAYPALISHIRING
-#
-# 1           9
-# P           H
-#
-# 2        8  10
-# A        S  I
-#
-# 3     7     11
-# Y     I     R
-#
-# 4  6        12 14
-# P  L        I  G
-#
-# 5           13
-# A           N
-#
-#
-# PHASIYIRPLIGAN
 # assert Solution().convert("PAYPALISHIRING", 5) == "PHASIYIRPLIGAN"
