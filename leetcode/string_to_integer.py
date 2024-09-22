737441ff6a6597f009cb16542c04bfddf868151e
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-22 12:18:56 +0900

    update: 0文字のエッジケース

--- leetcode/string_to_integer.py
+++ leetcode/string_to_integer.py
@@ -29,9 +29,11 @@ class Solution:
     """
 
     def myAtoi(self, s: str) -> int:
-        pass
+        if len(s) == 0:
+            return 0
 
 
+assert Solution().myAtoi("") == 0
 assert Solution().myAtoi("42") == 42
 assert Solution().myAtoi("-042") == -42
 assert Solution().myAtoi("1337c0d3") == 1337
