3a4b770accd7547af3575dbe07e0f2d1289c96e9
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-22 13:49:48 +0900

    add: これはeasyだから自力でいきたい

---
+++ leetcode/palindrome_number.py
@@ -0,0 +1,11 @@
+class Solution:
+    """9. Palindrome Number
+    Given an integer x, return true if x is a palindrome, and false otherwise.
+    整数 x を指定すると、x が回文の場合は true を返し、それ以外の場合は false を返します。
+    """
+    def isPalindrome(self, x: int) -> bool:
+        pass
+
+assert Solution().isPalindrome(121)
+assert not Solution().isPalindrome(-121)
+assert not Solution().isPalindrome(10)
