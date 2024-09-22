97597f36d0f7876bce65f73f9029f6f6712d35b8
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-22 13:53:15 +0900

    update: 文字列の反転の仕方を覚えたお陰でスピードアップしている

--- leetcode/palindrome_number.py
+++ leetcode/palindrome_number.py
@@ -4,7 +4,11 @@ class Solution:
     整数 x を指定すると、x が回文の場合は true を返し、それ以外の場合は false を返します。
     """
     def isPalindrome(self, x: int) -> bool:
-        pass
+        if x < 0:
+            return False
+        if str(x) == str(x)[::-1]:
+            return True
+        return False
 
 assert Solution().isPalindrome(121)
 assert not Solution().isPalindrome(-121)
