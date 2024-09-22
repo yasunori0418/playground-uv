ac8830121f76d441c8393c9c566ebde5fc756df0
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-22 11:56:30 +0900

    update: べき乗を使ってスマートに32bitの最大値を取得しておく

--- leetcode/reverse_integer.py
+++ leetcode/reverse_integer.py
@@ -10,7 +10,7 @@ class Solution:
     def reverse(self, x: int) -> int:
         abs_num = abs(x)
         reversed_num = int(str(abs_num)[::-1])
-        if reversed_num >= 2_147_483_647:
+        if reversed_num >= 2**31 - 1:
             return 0
         if x < 0:
             return -reversed_num
