a90fdb5e94e2cedb7d80dbbb53d05c64dff17df9
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-22 11:29:34 +0900

    update: 変に難しく考えすぎる癖があるな…

--- leetcode/zigzag_conversion.py
+++ leetcode/zigzag_conversion.py
@@ -1,59 +1,26 @@
 class Solution:
+    """
+        refer: https://qiita.com/KueharX/items/777868c9914317a65cd2
+    """
     def convert(self, s: str, numRows: int) -> str:
-        if len(s) <= 1:
+        if numRows <= 1:
             return s
+        num = 0
+        step = 0
+        results = [""] * numRows
+        for i in s:
+            results[num] += i
+            if num == 0:
+                step = 1
+            elif num == numRows - 1:
+                step = -1
+            num += step
+        return "".join(results)
 
 
 assert Solution().convert("A", 1) == "A"
-
-
-def gen_patterns(string: str, numRows):
-    pass
-
-
-assert gen_patterns("PAYPALISHIRING", 3) == [
-    ["P", "A", "Y"],
-    [" ", "P", " "],
-    ["A", "L", "I"],
-    [" ", "S", " "],
-    ["H", "I", "R"],
-    [" ", "I", " "],
-    ["N", "G"],
-]
-
-
-assert gen_patterns("PAYPALISHIRING", 3) == {
-    1: ["P", "A", "Y"],
-    2: [" ", "P", " "],
-    3: ["A", "L", "I"],
-    4: [" ", "S", " "],
-    5: ["H", "I", "R"],
-    6: [" ", "I", " "],
-    7: ["N", "G"],
-}
-
-
-assert gen_patterns("PAYPALISHIRING", 4) == {
-    1: ["P", "A", "Y", "P"],
-    2: [" ", " ", "A", " "],
-    3: [" ", "L", " ", " "],
-    4: ["I", "S", "H", "I"],
-    5: [" ", " ", "R", " "],
-    6: [" ", "I", " ", " "],
-    7: ["N", "G"],
-}
-
-
-assert gen_patterns("PAYPALISHIRING", 5) == {
-    1: ["P", "A", "Y", "P", "A"],
-    2: [" ", " ", " ", "L", " "],
-    3: [" ", " ", "I", " ", " "],
-    4: [" ", "S", " ", " ", " "],
-    5: ["H", "I", "R", "I", "N"],
-    6: [" ", " ", " ", "G"],
-}
-
-
-# assert Solution().convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR"
-# assert Solution().convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI"
-# assert Solution().convert("PAYPALISHIRING", 5) == "PHASIYIRPLIGAN"
+assert Solution().convert("AB", 1) == "AB"
+assert Solution().convert("ABC", 1) == "ABC"
+assert Solution().convert("PAYPALISHIRING", 3) == "PAHNAPLSIIGYIR"
+assert Solution().convert("PAYPALISHIRING", 4) == "PINALSIGYAHRPI"
+assert Solution().convert("PAYPALISHIRING", 5) == "PHASIYIRPLIGAN"
