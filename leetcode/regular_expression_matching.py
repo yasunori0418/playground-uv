b589e564456220bfe701269c404329cbf040182a
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-22 14:00:15 +0900

    Add: hardだけど頑張る

---
+++ leetcode/regular_expression_matching.py
@@ -0,0 +1,24 @@
+class Solution:
+    """10. Regular Expression Matching
+    Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+
+    '.' Matches any single character.
+    '*' Matches zero or more of the preceding element.
+    The matching should cover the entire input string (not partial).
+
+    ---
+
+    入力文字列 s とパターン p を指定して、「.」をサポートする正規表現マッチングを実装します。 および「*」の場合:
+
+    '.' 任意の1文字と一致します。
+    '*' 0個以上の先行要素と一致します。
+    一致は入力文字列全体 (部分的ではなく) をカバーする必要があります。
+    """
+
+    def isMatch(self, s: str, p: str) -> bool:
+        pass
+
+
+assert not Solution().isMatch("aa", "a")
+assert Solution().isMatch("aa", "a*")
+assert Solution().isMatch("ab", ".*")
