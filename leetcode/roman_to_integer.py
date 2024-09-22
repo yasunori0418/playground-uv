d30e55309760b4d1fecd661d54dd805b29dd974d
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-22 20:38:08 +0900

    Add: roman_to_integer 前の問題の逆をやる

---
+++ leetcode/roman_to_integer.py
@@ -0,0 +1,8 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        pass
+
+assert Solution().romanToInt("III") == 3
+assert Solution().romanToInt("LVIII") == 58
+assert Solution().romanToInt("MCMXCIV") == 1994
+assert Solution().romanToInt("MMMDCCXLIX") == 3749
