c2fec8197cb26f39d7d80121e7deaad15372e72d
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-22 13:55:03 +0900

    update: そうか、これだけでも良いのか

--- leetcode/palindrome_number.py
+++ leetcode/palindrome_number.py
@@ -4,11 +4,7 @@ class Solution:
     整数 x を指定すると、x が回文の場合は true を返し、それ以外の場合は false を返します。
     """
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
-        if str(x) == str(x)[::-1]:
-            return True
-        return False
+        return str(x) == str(x)[::-1]
 
 assert Solution().isPalindrome(121)
 assert not Solution().isPalindrome(-121)
