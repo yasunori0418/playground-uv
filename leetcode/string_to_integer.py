9d2e37d3834be8302b8d160d22d4832b10acb7c8
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-22 12:25:50 +0900

    update: 先頭に空白スペース入ってなかった

--- leetcode/string_to_integer.py
+++ leetcode/string_to_integer.py
@@ -36,7 +36,7 @@ class Solution:
 
 assert Solution().myAtoi("") == 0
 assert Solution().myAtoi("42") == 42
-assert Solution().myAtoi("-042") == -42
+assert Solution().myAtoi(" -042") == -42
 assert Solution().myAtoi("1337c0d3") == 1337
 assert Solution().myAtoi("0-1") == 0
 assert Solution().myAtoi("0-1") == 0
