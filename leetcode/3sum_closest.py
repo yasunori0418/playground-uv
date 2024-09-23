b63e187ac83354ce2f44a460c5da7787ca1661dd
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-23 22:40:11 +0900

    update: 答え分からんくて、見ちゃった…

--- leetcode/3sum_closest.py
+++ leetcode/3sum_closest.py
@@ -15,7 +15,25 @@ class Solution:
     """
 
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        pass
+        """結局分からんくて、答え見た
+        :refer: https://leetcode.com/problems/3sum-closest/solutions/5446457/2-ways-to-the-point-explanation-java-c-python/
+        """
+        sorted_nums = sorted(nums)
+        ans = float("inf")
+        for i in range(len(nums) - 2):
+            left = i + 1
+            right = len(nums) - 1
+            while left < right:
+                current_sum = sorted_nums[i] + sorted_nums[left] + sorted_nums[right]
+                if abs(current_sum - target) < abs(ans - target):
+                    ans = current_sum
+                if current_sum < target:
+                    left += 1
+                elif current_sum > target:
+                    right -= 1
+                else:
+                    return current_sum
+        return int(ans)
 
 
 assert Solution().threeSumClosest([-1, 2, 1, -4], 1) == 2
