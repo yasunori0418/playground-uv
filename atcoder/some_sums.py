5e22bd255dbd46d5716b321be06fc0d857f3e165
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-23 19:37:57 +0900

    update: 手元のテストは上手く動くけど、提出すると実行時エラーだけが出て何も分からん

--- atcoder/some_sums.py
+++ atcoder/some_sums.py
@@ -1,11 +1,26 @@
 def probrem():
-    print(some_sums(int(input()), int(input()), int(input())))
+    print(some_sums(input(), input(), input()))
 
 
-def some_sums(N: int, A: int, B: int):
-    pass
+def some_sums(N: str, A: str, B: str):
+    total = int(N)
+    min = int(A)
+    max = int(B)
+    ans = []
+    for i in range(1, total + 1):
+        buf = adder(i)
+        if buf >= min and buf <= max:
+            ans.append(i)
+    return sum(ans)
 
 
-assert some_sums(20, 2, 5) == 84
-assert some_sums(10, 1, 2) == 13
-assert some_sums(100, 4, 16) == 4554
+def adder(n: int) -> int:
+    buf = 0
+    for i in str(n):
+        buf += int(i)
+    return buf
+
+
+assert some_sums("20", "2", "5") == 84
+assert some_sums("10", "1", "2") == 13
+assert some_sums("100", "4", "16") == 4554
