f5a76aee42e078c82e92c2d3394dc3ba5341c7bc
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-23 19:14:51 +0900

    add: some_sums

---
+++ atcoder/some_sums.py
@@ -0,0 +1,11 @@
+def probrem():
+    print(some_sums(int(input()), int(input()), int(input())))
+
+
+def some_sums(N: int, A: int, B: int):
+    pass
+
+
+assert some_sums(20, 2, 5) == 84
+assert some_sums(10, 1, 2) == 13
+assert some_sums(100, 4, 16) == 4554
