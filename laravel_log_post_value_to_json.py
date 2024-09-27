940f73a84d197f61722711976be4311d2bae80eb
yasunori0418 <t-watanabe@gizumo-inc.jp>
2024-09-27 19:53:56 +0900

    update: prefixを丁寧に確認していくスタイル

--- laravel_log_post_value_to_json.py
+++ laravel_log_post_value_to_json.py
@@ -13,10 +13,13 @@ prefix:
   i: 整数
 """
 
+from typing import Literal, Optional
 import pytest
 
 
 class PostValue:
+    PREFIX_TYPES = Literal["Array", "String", "Integer"]
+
     def __init__(self, value: str) -> None:
         self.value = value
 
@@ -26,10 +29,42 @@ class PostValue:
         再帰的な括弧の文字列抽出ではなく、浅いデータ抽出
         :param: data str
         """
-        pass
+        open_parent_index = data.find("{") + 1
+        close_parent_index = data.rfind("}")
+        return data[open_parent_index:close_parent_index]
+
+    def _prefix_checker(self, data: str) -> Optional[PREFIX_TYPES]:
+        """dataの先頭文字列を識別"""
+        if data[0] == "a":
+            return "Array"
+        elif data[0] == "s":
+            return "String"
+        elif data[0] == "i":
+            return "Integer"
+        else:
+            return None
 
     def parse2dict(self) -> dict:
         return dict()
+
+
+@pytest.mark.parametrize(
+    [
+        "value",
+        "expected",
+    ],
+    [
+        pytest.param(
+            'a:3:{i:0;s:5:"item1";i:1;"item2";i:2;"item3"}',
+            "Array",
+        ),
+        pytest.param('i:0;s:5:"item1";i:1;"item2";i:2;"item3"', "Integer"),
+        pytest.param('s:3:"key";s:5:"value";', "String"),
+        pytest.param('n:3:"key";s:5:"value";', None),
+    ],
+)
+def test_prefix_checker(value: str, expected: str):
+    assert PostValue("test")._prefix_checker(value) == expected
 
 
 @pytest.mark.parametrize(
@@ -52,6 +87,7 @@ def test_extract_parent_data(value: str, expected: str):
     assert PostValue("test")._extract_parent_data(value) == expected
 
 
+@pytest.mark.skip(reason="未実装")
 @pytest.mark.parametrize(
     [
         "value",
