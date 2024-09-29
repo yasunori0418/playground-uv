7ac554f06c900bed98bd1c17ede16f1ef0ea9ad6
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-29 13:22:49 +0900

    update:

---
+++ leet2/two_sum.py
@@ -0,0 +1,12 @@
+"""1. Two Sum
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+整数 nums の配列と整数 target を指定すると、合計が target になるような 2 つの数値のインデックスを返します。
+各入力にはソリューションが 1 つだけ存在し、同じ要素を 2 回使用することはできないと想定できます。
+回答は任意の順序で返すことができます。
+"""
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        pass
