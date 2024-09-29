521d5ab434d025fa459cfc4e44093608a700c1ca
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-29 19:06:00 +0900

    add: add_two_numbers.py

---
+++ leet2/add_two_numbers.py
@@ -0,0 +1,9 @@
+from .utils.linked_list import ListNode, list2node, node2list
+from typing import Optional
+
+
+class Solution:
+    def addTwoNumbers(
+        self, l1: Optional[ListNode], l2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        return list2node([7,0,8])
---
+++ leet2/test_add_two_numbers.py
@@ -0,0 +1,9 @@
+from .add_two_numbers import Solution
+from .utils.linked_list import node2list, list2node
+
+
+def test_add_two_numbers():
+    assert node2list(
+        Solution().addTwoNumbers(list2node([2, 4, 3]), list2node([5, 6, 4]))
+    ) == [7, 0, 8]
+
