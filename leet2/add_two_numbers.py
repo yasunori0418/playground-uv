ca419e3265dc05f6ccf1538b8814515851bf1a1a
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-09-29 23:53:57 +0900

    update: 覚えていた答えは再帰でした

--- leet2/add_two_numbers.py
+++ leet2/add_two_numbers.py
@@ -1,9 +1,30 @@
-from .utils.linked_list import ListNode, list2node, node2list
+from .utils.linked_list import ListNode
 from typing import Optional
 
 
 class Solution:
     def addTwoNumbers(
-        self, l1: Optional[ListNode], l2: Optional[ListNode]
+        self,
+        l1: Optional[ListNode],
+        l2: Optional[ListNode],
+        carry: int = 0,
+        result_node: ListNode = ListNode(),
     ) -> Optional[ListNode]:
-        return list2node([7,0,8])
+        if not l1 and not l2 and not carry:
+            return None
+        temp_sum = l1.val if l1 else 0
+        temp_sum += l2.val if l2 else 0
+        summation = temp_sum + carry
+        num = summation % 10
+        new_carry = summation // 10
+        new_node = ListNode(num)
+        result_node.next = new_node
+        result_node = new_node
+        self.addTwoNumbers(
+            l1.next if l1 else None,
+            l2.next if l2 else None,
+            new_carry,
+            new_node
+        )
+        return result_node
+
--- leet2/test_add_two_numbers.py
+++ leet2/test_add_two_numbers.py
@@ -6,4 +6,6 @@ def test_add_two_numbers():
     assert node2list(
         Solution().addTwoNumbers(list2node([2, 4, 3]), list2node([5, 6, 4]))
     ) == [7, 0, 8]
-
+    assert node2list(
+        Solution().addTwoNumbers(list2node([0]), list2node([0]))
+    ) == [0]
