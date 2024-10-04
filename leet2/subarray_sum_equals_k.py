e3810463ddd4bbb31703e9f4986adbbfffa195d5
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-10-05 01:58:30 +0900

    update:

--- leet2/subarray_sum_equals_k.py
+++ leet2/subarray_sum_equals_k.py
@@ -8,6 +8,7 @@ class Solution:
 
 def solve(nums, k):
     # 辞書を初期化。キーは累積和、値はその累積和が出現した回数。
+    # 空の配列は必ず一つは含まれるため、初期化時に0をセットしている
     prefix_sum_count = {0: 1}
     current_sum = 0
     result = 0
@@ -18,6 +19,9 @@ def solve(nums, k):
         current_sum += num
 
         # current_sum - k が辞書に存在する場合、それは k になる部分配列があることを意味する
+        # current_sum - kは過去の累積和として存在した
+        # current_sumは現在の累積和
+        # current_sum - (current_sum - k) = 3 - (3 - 1) = 3 - 2 = k
         if current_sum - k in prefix_sum_count:
             result += prefix_sum_count[current_sum - k]
 
@@ -29,8 +33,29 @@ def solve(nums, k):
     return result
 
 
+def solve2(nums, k):
+    prefix_sums = [
+        0,
+    ]
+    result = 0
+
+    for n in nums:
+        # 累積和をそれぞれの段階まで計算
+        current_sum = prefix_sums[-1] + n
+
+        # 最新の累積和を除いた各累積和をチェック
+        for previous_sum in prefix_sums:
+            if current_sum - previous_sum == k:
+                result += 1
+
+        prefix_sums.append(current_sum)
+
+    return result
+
+
 # solve([1, 1, 1], 2)
-solve([1, 2, 3], 3)
+# solve([1, 2, 3], 3)
+solve([1, 1, 1, 1, 1, 1, 1, 1, 1, 1], 4)
 
 
 def test_solve():
