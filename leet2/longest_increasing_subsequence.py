e072ea9b358f418dc49569bccbb4e9773baaba9d
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-10-06 00:30:44 +0900

    add: longest_increasing_subsequence.py

---
+++ leet2/longest_increasing_subsequence.py
@@ -0,0 +1,12 @@
+"""300. Longest Increasing Subsequence
+https://leetcode.com/problems/longest-increasing-subsequence/description/
+Given an integer array nums, return the length of the longest strictly increasing subsequence.
+整数配列 nums を指定すると、厳密に増加する最長のサブシーケンスの長さを返します。
+"""
+
+
+def solve(nums: list[int]) -> int:
+    return 4
+
+
+assert solve([10, 9, 2, 5, 3, 7, 101, 18]) == 4
