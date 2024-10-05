1818eb8c108a1e95cc6c77b842c9d3f61627f4a5
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-10-06 00:54:34 +0900

    update:

--- leet2/longest_increasing_subsequence.py
+++ leet2/longest_increasing_subsequence.py
@@ -6,7 +6,17 @@ Given an integer array nums, return the length of the longest strictly increasin
 
 
 def solve(nums: list[int]) -> int:
-    return 4
+    if not nums:
+        return 0
+    nums_len = len(nums)
+    dp = [1] * nums_len
+    for i in range(1, nums_len):
+        for j in range(i):
+            if nums[j] < nums[i]:
+                dp[i] = max(dp[i], dp[j] + 1)
+    return max(dp)
 
 
 assert solve([10, 9, 2, 5, 3, 7, 101, 18]) == 4
+assert solve([0,1,0,3,2,3]) == 4
+assert solve([7,7,7,7,7,7,7]) == 1
