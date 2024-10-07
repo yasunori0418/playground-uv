be6d238a086f71fa09091aedb234c214620d8067
yasunori0418 <yasunori.kirin0418@gmail.com>
2024-10-07 22:52:49 +0900

    update: ほぼchatgptの答えを暗記している用な物…

---
+++ leet2/house_robber.py
@@ -0,0 +1,14 @@
+def solve(nums: list[int]) -> int:
+    if len(nums) == 1:
+        return nums[0]
+    dp = [0] * len(nums)
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+    for i in range(2, len(nums)):
+        dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+    return dp[-1]
+
+
+assert solve([1, 2, 3, 1]) == 4
+assert solve([2, 7, 9, 3, 1]) == 12
+# assert solve([3]) == 3
